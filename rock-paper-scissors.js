eb586f59871ed93bcba4d5d383415d23494c7743
Bilal Shaikh <vol1shaikh@gmail.com>
2025-06-29 15:11:40 +0530

    Add UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,19 +1,29 @@
-// function definition
+const body = document.querySelector("body");
+
+const rock = document.createElement("button");
+rock.textContent = "rock";
+body.appendChild(rock);
+
+const paper = document.createElement("button");
+paper.textContent = "paper";
+body.appendChild(paper);
+
+const scissors = document.createElement("button");
+scissors.textContent = "scissors";
+body.appendChild(scissors);
+
 function getComputerChoice() {
-    // store a random number from 1 to 100
+    
     let randomNumberOneToHundred = Math.floor(Math.random() * 100) + 1;
 
-    //if statement that returns rock if number is between 1 to 33
     if (randomNumberOneToHundred >= 1 && randomNumberOneToHundred <= 33) {
         return "rock";
     }
 
-    //if statement that returns paper if number is between 34 to 66
     if (randomNumberOneToHundred >= 34 && randomNumberOneToHundred <= 66) {
         return "paper";
     }
 
-    //if statement that returns scissors if number is between 67 to 100
     if (randomNumberOneToHundred >= 67 && randomNumberOneToHundred <= 100) {
         return "scissors";
 
@@ -21,126 +31,80 @@ function getComputerChoice() {
     }
 }
 
-//function definition
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, Scissors! What's your choice?");
-    return humanChoice;
-}
-
-
-
-
-
-//function definition
 function playGame() {
 
     //declaring variables for tracking scores
     let humanScore = 0;
     let computerScore = 0;
     let roundNumber = 1;
+    let message;
 
-        //function definition
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
 
-        //show round number and then increment it
-        console.log(`Round no: ${roundNumber}`);
-        roundNumber++
-
-        //their choices
-        console.log(`human choice is: ${humanChoice}`)
-        console.log(`computer choice is: ${computerChoice}`)
+        
 
         //human win conditions
         if(humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You Won! Rock beats Scissors");
+            message = "You Won! Rock beats Scissors";
             humanScore++;
         }
 
         else if(humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You Won! Paper beats Rock");
+            message = "You Won! Paper beats Rock";
             humanScore++;
         }
 
         else if(humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You Won! Scissors beats Paper");
+            message = "You Won! Scissors beats Paper";
             humanScore++;
         }
 
         //computer win conditions
         else if(computerChoice === "rock" && humanChoice === "scissors") {
-            console.log("You Lose! Rock beats Scissors");
+            message = "You Lose! Rock beats Scissors";
             computerScore++;
         }
 
         else if(computerChoice === "paper" && humanChoice === "rock") {
-            console.log("You Lose! Paper beats Rock");
+            message = "You Lose! Paper beats Rock";
             computerScore++;
         }
 
         else if(computerChoice === "scissors" && humanChoice === "paper") {
-            console.log("You Lose! Scissors beats Paper");
+            message = "You Lose! Scissors beats Paper";
             computerScore++;
         }
 
         //tie condition
         else {
-            console.log("It's a tie!");
+            message = "It's a tie!";
         }
 
-        console.log(`The human score is: ${humanScore}`);
-        console.log(`The computer score is: ${computerScore}`);
+        const humanResult = document.createElement("div")
+        humanResult.textContent = `Round no.: ${roundNumber}, ${message}
+        human choice: ${humanChoice}, computer choice: ${computerChoice},
+        Human score is: ${humanScore} & computer score is: ${computerScore}`;
+        body.appendChild(humanResult);
+
+        roundNumber++
+
+        if (humanScore == 5 || computerScore == 5) {
+            const winner = document.createElement("div");
+            if( humanScore == 5) winner.textContent = "Winner is the player";
+            else if(computerScore == 5) winner.textContent = "Winner is the computer";
+            return body.appendChild(winner)
+        }
     }
 
-    const humanSelection = getHumanChoice;
-    const computerSelection = getComputerChoice;
-
-    playRound(humanSelection(), computerSelection());
-    playRound(humanSelection(), computerSelection());
-    playRound(humanSelection(), computerSelection());
-
-    //condition to stop the game when one side wins the best of 5
-    if (humanScore === 3 || computerScore === 3) {
-        if (humanScore === 3) {
-            console.log("You have won the game. Congratulations!")
-        }
-        else if(computerScore === 3) {
-        console.log("You lost. Better luck next time!")
-        }
-        return;
-    }
-    playRound(humanSelection(), computerSelection());
-    if (humanScore === 3 || computerScore === 3) {
-        if (humanScore === 3) {
-            console.log("You have won the game. Congratulations!")
-        }
-        else if(computerScore === 3) {
-        console.log("You lost. Better luck next time!")
-        }
-        return;
-    }
-    playRound(humanSelection(), computerSelection());
-    if (humanScore === 3 || computerScore === 3) {
-        if (humanScore === 3) {
-            console.log("You have won the game. Congratulations!")
-        }
-        else if(computerScore === 3) {
-        console.log("You lost. Better luck next time!")
-        }
-        return;
-    }
-
-    //winner declaration condition in case tie conditions
-    
-    if(humanScore > computerScore) {
-        console.log("You have won the game. Congratulations!")
-    }
-    else if(computerScore > humanScore) {
-        console.log("You lost. Better luck next time!")
-    }
-    else {
-        console.log("Tie! Game ended.")
-    }
+    rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+    paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+    scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
 }
 
 playGame()
+
+//1. remove logic that plays exactly five rounds. Done.
+//2. create three buttons, rps. add event listeners that calls playRound for
+//for each click and correct player selection.
+//3. add a div for results and change all console.log into dom methods
